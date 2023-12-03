12744dfacef85260c935e12cb503867694a1aba1
Nicolas Smaniotto <nicolasantoniosmaniotto@gmail.com>
2023-12-02 21:49:39 -0300

    Envio do Projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+### Projeto feito durante op bootcamp da DIO
+## Projeto feito com base nas aulas do professor FELIPE na DIO
---
+++ src/scripts/engine.js
@@ -0,0 +1,35 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .keys")
+const volumeSlider = document.querySelector(".volume-slider input")
+const keysCheck = document.querySelector(".keys-check input")
+
+let audio = new Audio("tunes")
+let mapedKeys = []  
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`
+    audio.play()
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    clickedKey.classList.add(".active");
+    setTimeout(clickedKey.classList.remove('.active'), 150)
+
+}
+pianoKeys.forEach((key) => {
+    console.log(key.dataset.key)
+    key.addEventListener('click', () => playTune(key.dataset.key))
+    mapedKeys.push(key.dataset.key)
+})
+document.addEventListener("keydown", (e) => {
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key)
+
+    }
+})
+const handleVolume = (e)=>{
+    console.log(e.target.value)
+    audio.volume = e.target.value
+}
+const showHideKeys = ()=>{
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+keysCheck.addEventListener("click",showHideKeys)
+volumeSlider.addEventListener("input",handleVolume)
